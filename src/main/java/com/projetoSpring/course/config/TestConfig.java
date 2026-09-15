3c8583cd732febfafc1ad46ecee6189fbe98da89
Guilherme Noe <guilhermenoe2020@gmail.com>
2026-09-15 15:54:23 -0400

    payment, one-to-one association

--- src/main/java/com/projetoSpring/course/config/TestConfig.java
+++ src/main/java/com/projetoSpring/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetoSpring.course.entities.Category;
 import com.projetoSpring.course.entities.Order;
 import com.projetoSpring.course.entities.OrderItem;
+import com.projetoSpring.course.entities.Payment;
 import com.projetoSpring.course.entities.Product;
 import com.projetoSpring.course.entities.User;
 import com.projetoSpring.course.entities.enums.OrderStatus;
@@ -86,6 +87,12 @@ class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 		
 	}
 	
