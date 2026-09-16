9cf4e89ae1b0c30c4a2e653fa5e458b6ef871a11
Guilherme Noe <guilhermenoe2020@gmail.com>
2026-09-16 08:15:03 -0400

    user update

--- src/main/java/com/projetoSpring/course/services/UserService.java
+++ src/main/java/com/projetoSpring/course/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.projetoSpring.course.entities.User;
 import com.projetoSpring.course.repositories.UserRepository;
@@ -32,5 +33,19 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	@Transactional
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+	
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+	
 	
 }
