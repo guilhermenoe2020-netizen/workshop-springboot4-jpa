89ea2a136d1b9643b664e0644b2ca842000a94ea
Guilherme Noe <guilhermenoe2020@gmail.com>
2026-09-16 14:58:38 -0400

    exception handling - delete

--- src/main/java/com/projetoSpring/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/projetoSpring/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.projetoSpring.course.services.exceptions.DataBaseException;
 import com.projetoSpring.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,4 +22,15 @@ public class ResourceExceptionHandler{
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request) {
+		String error = "database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	
+	
 }
--- src/main/java/com/projetoSpring/course/services/UserService.java
+++ src/main/java/com/projetoSpring/course/services/UserService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.projetoSpring.course.entities.User;
 import com.projetoSpring.course.repositories.UserRepository;
+import com.projetoSpring.course.services.exceptions.DataBaseException;
 import com.projetoSpring.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -32,8 +34,17 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
-	}
+	  if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException(id);
+	  }
+		 try{
+	       repository.deleteById(id);
+		 }
+	     catch(DataIntegrityViolationException e) {
+		   throw new DataBaseException(e.getMessage());
+	     }
+	  
+	 }
 	
 	@Transactional
 	public User update(Long id, User obj) {
